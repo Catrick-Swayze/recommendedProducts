bc47eb31c06d1fff707781d9a2009eb7cb6197ac
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-15 10:48:04 -0400

    Implement other random data generating functions

--- postgres/generateData.js
+++ postgres/generateData.js
@@ -6,9 +6,18 @@ var randomTitle = () => {
 }
 
 // Random Price (between 19.99 and 139.99)
+var randomPrice = () => {
+  return Math.floor(Math.random(139 - 19) + 19) + 0.99;
+}
 
 // Random Image (fakerJS, but replace with S3)
+var randomImage = () => {
+  return faker.image.imageURL();
+}
 
 // Similarity Rating (1-100)
+var randomSimilarityRating = () => {
+  return Math.floor(Math.random(100 - 1) + 1);
+}
 
-module.exports =
+module.exports = { randomTitle, randomPrice, randomImage, randomSimilarityRating };
