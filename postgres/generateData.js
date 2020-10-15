de78705a26503d7a316f4e5a8ab8a8facd693f38
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-15 10:30:20 -0400

    Create new folder for postgres funcitonality, write first random data generation function

---
+++ postgres/generateData.js
@@ -0,0 +1,14 @@
+const faker = require('faker');
+
+// Random Title (fakerJS)
+var randomTitle = () => {
+  return faker.commerce.productName();
+}
+
+// Random Price (between 19.99 and 139.99)
+
+// Random Image (fakerJS, but replace with S3)
+
+// Similarity Rating (1-100)
+
+module.exports =
