858d12cbccccb3a5c9f694b7cd32942a722ad4f3
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-24 14:50:53 -0400

    Reconfigured data generating script and populated new postgres table as expected

--- postgres/seed_helpers.js
+++ postgres/seed_helpers.js
@@ -16,20 +16,23 @@ var randomImage = (int) => {
   return `https://twzkraus-fec-images.s3-us-west-1.amazonaws.com/target-images/${int % 50}.jpg`;
 }
 
-// Similarity Rating (1-100)
-var randomSimilarityRating = () => {
-  return Math.floor(Math.random() * 100 + 1);
-}
-
 const csvWriter = createCsvWriter({
     path: './postgres/csvFiles/sdc.csv',
     header: [
         {id: 'id', title: 'Id'},
         {id: 'title', title: 'Title'},
+        {id: 'brand', title: 'Brand'},
+        {id: 'department', title: 'Department'},
         {id: 'price', title: 'Price'},
         {id: 'imageurl', title: 'ImageURL'},
-        {id: 'similarityScore', title: 'Similarity Score'}
+        {id: 'producturl', title: 'ProductURL'}
     ]
 });
 
-module.exports = { randomTitle, randomPrice, randomImage, randomSimilarityRating, csvWriter };
+
+// Similarity Rating (1-100)
+// var randomSimilarityRating = () => {
+//   return Math.floor(Math.random() * 100 + 1);
+// }
+
+module.exports = { randomTitle, randomPrice, randomImage, csvWriter };
