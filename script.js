4577b6a90b74ff6e603a5901e985937224c83eb9
Chris Cali <ChristopherCali1415@gmail.com>
2020-10-26 01:13:55 -0400

    GET and POST both in newrelic script.js file

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 export let options = {
   scenarios: {
-      constant_request_rate: {
-          executor: 'constant-arrival-rate',
-          rate: 1000,
-          timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
-          duration: '60s',
-          preAllocatedVUs: 100, // how large the initial pool of VUs would be
-          maxVUs: 400, // if the preAllocatedVUs are not enough, we can initialize more
-      }
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 1000,
+      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+      duration: '60s',
+      preAllocatedVUs: 100, // how large the initial pool of VUs would be
+      maxVUs: 400, // if the preAllocatedVUs are not enough, we can initialize more
+    }
   }
 };
 
 export default function () {
   // GET
-  //http.get('http://localhost:3030/products/id/1'); // these are the endpoints I'll be hitting
+  http.get('http://localhost:3030/products/id/1'); // these are the endpoints I'll be hitting
 
   // POST
   var url = 'http://localhost:3030/newProduct';
